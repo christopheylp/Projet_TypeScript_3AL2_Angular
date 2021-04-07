c3fef11bdc9aac8a5a8f2f6a25405de30a1e02cf
christopheylp <christopheyalap957@gmail.com>
2021-04-07 13:01:03 +0200

    Added attack buty not functionnal

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import {Pokemon} from "./pokemon";
 const pokemon1 = new Pokemon({name:"poke1"});
 const pokemon2 = new Pokemon({name:"poke2"});
 
+runFight(pokemon1,pokemon2);
+
 export function firstAttack(poke1: Pokemon, poke2: Pokemon, ):Pokemon {
     if(poke1.speed > poke2.speed){
         return poke1;
@@ -13,5 +15,23 @@ export function firstAttack(poke1: Pokemon, poke2: Pokemon, ):Pokemon {
     return poke1;
 }
 
-const first = firstAttack(pokemon1,pokemon2);
-console.log("Le premier attaquant est "+first.name+" avec "+first.health+" points de vie et une vitesse de "+first.speed+". \n");
+export function runFight(p1: Pokemon, p2: Pokemon){
+    const first = firstAttack(p1,p2);
+    if(first === p1){
+        var adv = p2;
+    }else{
+        var adv = p1;
+    }
+    console.log(adv.name+"\npoints de vie: "+adv.health+"\nvitesse: "+adv.speed+". \n");
+    console.log(first.name+"\npoints de vie: "+first.health+"\nvitesse: "+first.speed+". \n");
+    let turn = 0;
+    while(first.health>=0 && adv.health>=0){
+        if(turn%2 == 0){
+            adv.attackPokemon(first.attack);
+        }else{
+            first.attackPokemon(adv.attack);
+        }
+    }
+    console.log(adv.name+"\npoints de vie: "+adv.health+"\nvitesse: "+adv.speed+". \n");
+    console.log(first.name+"\npoints de vie: "+first.health+"\nvitesse: "+first.speed+". \n");
+}
--- pokemon.ts
+++ pokemon.ts
@@ -2,6 +2,7 @@ interface IPokemon{
     name: string;
     health?: number;
     speed?: number;
+    attack?: number;
 }
 
 export class Pokemon implements IPokemon {
@@ -9,11 +10,17 @@ export class Pokemon implements IPokemon {
     name: string;
     health: number;
     speed: number;
+    attack: number;
 
     constructor(props :IPokemon) {
         this.name = props.name;
         this.health = Math.floor(Math.random() * 200);
         this.speed = props.speed || Math.floor(Math.random() * 20);
+        this.attack = props.attack || Math.floor(Math.random() * 100);
+    }
+
+    attackPokemon(advAttack: number){
+        this.attack-=advAttack;
     }
 
 }
